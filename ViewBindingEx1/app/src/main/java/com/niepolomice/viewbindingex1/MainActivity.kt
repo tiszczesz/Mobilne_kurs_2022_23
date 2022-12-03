66c7e94934db4d9f558201531669fde43145439b
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2022-12-03 10:18:51 +0100

    ff

--- ViewBindingEx1/app/src/main/java/com/niepolomice/viewbindingex1/MainActivity.kt
+++ ViewBindingEx1/app/src/main/java/com/niepolomice/viewbindingex1/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         bind = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bind.root)
+
         bind.btnFirstname.setOnClickListener {
             if (bind.etFirstName.text.toString().trim().isEmpty()) {
                 bind.tvResult.text = "Podaj dane"
@@ -23,6 +24,30 @@ class MainActivity : AppCompatActivity() {
                 "odwracalne imie"
             } else {
                 "nieodwracalne imie"
+            }
+        }
+        bind.btnLastname.setOnClickListener {
+            val lastName = bind.edLastname.text.toString();
+            if(lastName.isNullOrEmpty()){
+                bind.tvResult.text = "Brak nazwiska!!!"
+                return@setOnClickListener
+            }
+            if(StringOperations.IsOnlyLetter(bind.edLastname.text.toString())){
+                bind.tvResult.text = "wszystkie litery w nazwisku"
+            }else{
+                bind.tvResult.text = "nie wszystkie to litery w nazwisku"
+            }
+        }
+        bind.btnEmail.setOnClickListener {
+            val email = bind.edEmail.text.toString().trim()
+            if(email.isNullOrEmpty()){
+                bind.tvResult.text = "Brak emaila!!!"
+                return@setOnClickListener
+            }
+            if(StringOperations.IsEmail(bind.edEmail.toString())){
+                bind.tvResult.text = "Poprawny adres emailowy";
+            }else{
+                bind.tvResult.text = "Błędny adres emailowy";
             }
         }
     }
--- ViewBindingEx1/app/src/main/java/com/niepolomice/viewbindingex1/StringOperations.kt
+++ ViewBindingEx1/app/src/main/java/com/niepolomice/viewbindingex1/StringOperations.kt
@@ -8,5 +8,16 @@ class StringOperations {
         fun IsPalindrom(text:String):Boolean {
             return if(Reverse(text.toString().trim().lowercase()).equals(text.toString().trim().lowercase())){ true } else {false}
         }
+        fun IsOnlyLetter(text:String):Boolean {
+            for (letter in text){
+                if(!letter.isLetter()) {
+                   return false
+                }
+            }
+            return true
+        }
+        fun IsEmail(text:String):Boolean{
+            return text.contains("@")
+        }
     }
 }
