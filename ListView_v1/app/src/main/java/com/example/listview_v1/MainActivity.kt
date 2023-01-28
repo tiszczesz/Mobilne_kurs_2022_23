0c8f516a21deb8f7a88e505c3054601ea67dc2fb
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2023-01-28 10:03:35 +0100

    hhh

---
+++ ListView_v1/app/src/main/java/com/example/listview_v1/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.listview_v1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import com.example.listview_v1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val myTodo = arrayListOf<String>("Zjeść śniadanie","Umyć się",
+            "Ubrać się","Zagadać do domowników","Czas wolny")
+        val listAdapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,myTodo);
+        binding.listView.adapter = listAdapter
+        binding.listView.onItemClickListener = AdapterView.OnItemClickListener{
+            adapterView, view, position, id ->
+            val selectedItem = adapterView.getItemAtPosition(position) as String
+            val itemIdAtPos = adapterView.getItemIdAtPosition(position)
+            Toast.makeText(this@MainActivity,"kliknięto: $selectedItem o indeksie: $itemIdAtPos",
+            Toast.LENGTH_SHORT).show()
+        }
+        binding.btnAdd.setOnClickListener {
+            val item = binding.addNew.text.toString()
+            if (item.isEmpty()) return@setOnClickListener;
+            myTodo.add(item)
+            listAdapter.notifyDataSetChanged()
+        }
+
+    }
+}
