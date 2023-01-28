806a77bbf67fc686bb6b6f6d545f7f767c88238a
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2023-01-28 12:26:50 +0100

    fff

--- RecycledView_v1/app/src/main/java/com/example/recycledview_v1/MainActivity.kt
+++ RecycledView_v1/app/src/main/java/com/example/recycledview_v1/MainActivity.kt
@@ -1,12 +1,11 @@
-package com.example.multiactivity
+package com.example.recycledview_v1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class SecondActivityActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.second_activity)
+        setContentView(R.layout.activity_main)
     }
 }
